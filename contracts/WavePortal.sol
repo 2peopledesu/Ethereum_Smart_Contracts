54ec453dd72a5dfc6394594d24036ee4b5dd4b13
2peopledesu <2peopledesu@gmail.com>
2022-03-03 15:05:58 -0800

    test

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("My fist Smart Contract");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }

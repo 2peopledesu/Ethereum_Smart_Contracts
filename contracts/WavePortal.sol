ca2f97325d7d1cb3f2a87c8c1e6d3b662e7925d0
2peopledesu <2peopledesu@gmail.com>
2022-03-05 04:08:20 -0800

    added message

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,13 +5,33 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("My fist Smart Contract");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who Waved
+        string message; // The message the user sent
+        uint256 timestamp; // The timestamp when the user waved
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    constructor() {
+        console.log("My first Smart Contract");
+    }
+
+    function wave(string memory _message) public {
+        
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        
+        console.log("%s waved w/ message %s", msg.sender, _message);
+        
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message); 
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,26 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-
+    console.log("Contract addy:", waveContract.address);
+  
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
-
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-};
+    console.log(waveCount.toNumber());
+  
+    /**
+     * Let's send a few waves!
+     */
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
+  
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
+  
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+  };
   
   const runMain = async () => {
     try {
